8c59bea9968244584da7e278b23505cc189e7da7
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2025-11-03 13:46:53 +0100

    info from host component

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,5 +11,21 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './preview.component.scss'
 })
 export class PreviewComponent {
+  @Input() private hostComponent!: any;
+  
+  constructor() {}
 
+  get previewUrl(): string | undefined {
+    //console.log(this.hostComponent.searchResult['@id']);
+    return this.hostComponent?.searchResult?.['@id'];
+  }
+
+  openPreview(): void {
+    const url = this.previewUrl;
+    if (!url) {
+      return;
+    }
+
+    window.open(url, '_blank', 'noopener');
+  }
 }
